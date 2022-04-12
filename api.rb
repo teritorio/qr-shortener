68b09cb2f351336322fae937f0a21fc2a50fc950
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2022-04-12 14:35:29 +0200

    Review API

--- api.rb
+++ api.rb
@@ -3,27 +3,26 @@ require 'sinatra'
 require './shortener'
 require './qrcode'
 
+get '/shorten' do
+  url = params[:url]
+  shorten(ENV['URL_BASE'], url) if url && url.size < 1024
+end
+
+get '/qrcode.svg' do
+  url = params[:url]
+  if url && url.size < 1024
+    url = shorten(ENV['URL_BASE'], url)
+
+    content_type 'image/svg+xml'
+    qrcode(url)
+  end
+end
+
 get '/:short' do
   url = unshorten(params[:short])
   if url.nil?
     404
   else
     redirect url
-  end
-end
-
-get %r{/shorten/.*} do
-  # REQUEST_URI
-  url = request.env['REQUEST_PATH'][('/shorten/'.size)..-1]
-  shorten(ENV['URL_BASE'], url) if url && url.size < 1024
-end
-
-get %r{/qrcode.svg/.*} do
-  url = request.env['REQUEST_PATH'][('/shorten/'.size)..-1]
-  if url && url.size < 1024
-    url = shorten(ENV['URL_BASE'], url)
-
-    content_type 'image/svg+xml'
-    qrcode(url)
   end
 end
